a15c80164905b6115850c498f47c4040a59f258d
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2023-10-17 16:51:41 -0700

    adding resource tracker

---
+++ code/aind_smartspim_destripe/utils/utils.py
@@ -0,0 +1,165 @@
+"""
+Utility functions
+"""
+import datetime
+import logging
+import multiprocessing
+import time
+from typing import List
+
+import matplotlib.pyplot as plt
+import psutil
+
+
+def profile_resources(
+    time_points: List,
+    cpu_percentages: List,
+    memory_usages: List,
+    monitoring_interval: int,
+):
+    """
+    Profiles compute resources usage.
+
+    Parameters
+    ----------
+    time_points: List
+        List to save all the time points
+        collected
+
+    cpu_percentages: List
+        List to save the cpu percentages
+        during the execution
+
+    memory_usage: List
+        List to save the memory usage
+        percentages during the execution
+
+    monitoring_interval: int
+        Monitoring interval in seconds
+    """
+    start_time = time.time()
+
+    while True:
+        current_time = time.time() - start_time
+        time_points.append(current_time)
+
+        # CPU Usage
+        cpu_percent = psutil.cpu_percent(interval=monitoring_interval)
+        cpu_percentages.append(cpu_percent)
+
+        # Memory usage
+        memory_info = psutil.virtual_memory()
+        memory_usages.append(memory_info.percent)
+
+        time.sleep(monitoring_interval)
+
+
+def generate_resources_graphs(
+    time_points: List,
+    cpu_percentages: List,
+    memory_usages: List,
+    output_path: str,
+    prefix: str,
+):
+    """
+    Profiles compute resources usage.
+
+    Parameters
+    ----------
+    time_points: List
+        List to save all the time points
+        collected
+
+    cpu_percentages: List
+        List to save the cpu percentages
+        during the execution
+
+    memory_usage: List
+        List to save the memory usage
+        percentages during the execution
+
+    output_path: str
+        Path where the image will be saved
+
+    prefix: str
+        Prefix name for the image
+    """
+    time_len = len(time_points)
+    memory_len = len(memory_usages)
+    cpu_len = len(cpu_percentages)
+
+    min_len = min([time_len, memory_len, cpu_len])
+    if not min_len:
+        return
+
+    plt.figure(figsize=(10, 6))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(time_points, cpu_percentages, label="CPU Usage")
+    plt.xlabel("Time (s)")
+    plt.ylabel("CPU Usage (%)")
+    plt.title("CPU Usage Over Time")
+    plt.grid(True)
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(time_points, memory_usages, label="Memory Usage")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Memory Usage (%)")
+    plt.title("Memory Usage Over Time")
+    plt.grid(True)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig(f"{output_path}/{prefix}_compute_resources.png", bbox_inches="tight")
+
+
+def stop_child_process(process: multiprocessing.Process):
+    """
+    Stops a process
+
+    Parameters
+    ----------
+    process: multiprocessing.Process
+        Process to stop
+    """
+    process.terminate()
+    process.join()
+
+
+def create_logger(output_log_path: str) -> logging.Logger:
+    """
+    Creates a logger that generates
+    output logs to a specific path.
+
+    Parameters
+    ------------
+    output_log_path: PathLike
+        Path where the log is going
+        to be stored
+
+    Returns
+    -----------
+    logging.Logger
+        Created logger pointing to
+        the file path.
+    """
+    CURR_DATE_TIME = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    LOGS_FILE = f"{output_log_path}/destripe_log_{CURR_DATE_TIME}.log"
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s : %(message)s",
+        datefmt="%Y-%m-%d %H:%M",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(LOGS_FILE, "a"),
+        ],
+        force=True,
+    )
+
+    logging.disable("DEBUG")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    return logger
