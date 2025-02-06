f8b5494e062f9cf1630cd0bfa4ba1d33f315af4b
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-02-05 20:21:55 -0800

    improving dockerfile and init

--- code/aind_smartspim_destripe/__init__.py
+++ code/aind_smartspim_destripe/__init__.py
@@ -1,3 +1,21 @@
 """SmartSPIM destriper version."""
 
 __version__ = "0.0.4"
+__authors__ = ["Camilo Laiton"]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = (
+    "Image preprocessing for flatfield correction and "
+    "image destriping"
+)
+__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-destripe"
+
+__maintainers__ = ["Camilo Laiton"]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-smartspim-destripe"
+__status__ = "Production"  # 'Development' 'Production', 'Beta'
+__pipeline_version__ = "3.0.0"
