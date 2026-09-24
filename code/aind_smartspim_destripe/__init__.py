a4d6709927cc7bebd7c8195434b40f4388970008
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2026-09-24 11:33:24 -0700

    bumping pipeline version

--- code/aind_smartspim_destripe/__init__.py
+++ code/aind_smartspim_destripe/__init__.py
@@ -16,4 +16,4 @@ __maintainer_emails__ = [
 __title__ = "aind-smartspim-destripe"
 __pipeline_name__ = "SmartSPIM Pipeline"
 __status__ = "Production"  # 'Development' 'Production', 'Beta'
-__pipeline_version__ = "5.0.0"
+__pipeline_version__ = "6.0.0"
